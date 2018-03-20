09e58c20c5fad8c4bfac896fed77fe1a9c96e605
Sasha Demyanik <ttsvan@msn.com>
2018-03-19 22:12:21 -0700

    Adds in hover function for header buttons.

--- code-player/script.js
+++ code-player/script.js
@@ -3,6 +3,6 @@ Description: JavaScript file for Code Player html
 Author: Sasha Demyanik
 */
 
-$( function() {
-
+$(".toggle-button").hover(function() {
+    $(this).css("background-color", "blue");
 });
