403da51e415bb469f5cc22411e6a9275d83151b1
Sasha Demyanik <ttsvan@msn.com>
2018-03-20 18:55:12 -0700

    Adds css and javascript panels, implements dynamic sizing.

--- code-player/script.js
+++ code-player/script.js
@@ -19,25 +19,30 @@ $(function() {
         $(this).toggleClass("active");
 
         $(this).removeClass("highlighted-button");
+
+        //Retrieve the clicked panel id and toggle the hidden class
+        var panelID = $(this).attr("id") + "-panel";
+        $("#" + panelID).toggleClass("hidden");
+
+        //Get the total number of panels and subtract hidden panels
+        //to calculate the total active panels
+        var numPanels = $(".panel").length - $(".hidden").length;
+
+        //Adjust the width of the panel based on active panels
+        $(".panel").width(($(window).width() / numPanels) - 10);
     });
 
 
     //Set panel height and width to fit within the window
     $(".panel").height($(window).height() - $("#header").height() - 15);
-
     $(".panel").width(($(window).width() / 2) - 10);
 
 
     //Set the iframe contents to contain the html panel values
     $("iframe").contents().find("html").html($("#html-panel").val());
 
-    //On change to html-panel, set the iframe text only if different
+    //On change to html-panel, set the iframe text
     $("#html-panel").on("change keyup paste", function() {
-        var currentVal = $(this).val();
-        var iframeText = $("iframe").contents().find("html");
-        if (currentVal == iframeText) {
-            return;
-        }
 
         $("iframe").contents().find("html").html($("#html-panel").val());
 
